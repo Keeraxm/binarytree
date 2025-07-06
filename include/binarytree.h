87aab8a57a22d1d13a169be3215b29312d89775e
Keera <jesper.tennevik@gmail.com>
2025-07-07 01:04:42 +0200

    first nodes

--- include/binarytree.h
+++ include/binarytree.h
@@ -1,6 +1,12 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
+typedef struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+} node_b;
 
+node_b* create_node(int data);
 
 #endif
--- src/binarytree.c
+++ src/binarytree.c
@@ -1,3 +1,21 @@
+#include <binarytree.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(){
+    node_b* my_root = create_node(0);
+
     return 0;
 }
+
+node_b* create_node(int data){
+    node_b* root = (node_b*) malloc(sizeof(node_b*));
+    
+    root->data = data;
+    root->left = NULL;
+    root->right = NULL;
+    
+    return root;
+}
+
+
